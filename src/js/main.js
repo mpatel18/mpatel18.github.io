99b024037909aeb144462cb97afc953f77198046
Meekit Patel <meekitpatel@gmail.com>
2020-10-01 14:51:20 -0400

    add new hexconnect img

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,26 @@
+// Function to Scroll Document to the top
 function returnTop(){
     document.body.scrollTop = 0; 
     document.documentElement.scrollTop = 0; 
 }
+
+// Function to loop through images 
+// var i = 0; 
+// var img_array = []; 
+// var speed = 3000; 
+
+// img_array[0] = 'src/img/cover_crop_img1.PNG';
+// img_array[1] = 'src/img/cover_crop_img2.PNG';
+// img_array[2] = 'src/img/cover_crop_img3.PNG';
+
+// function loopThrough() {
+//     document.slide.src = img_array[i]; 
+//     if(i < img_array.length - 1){
+//         i++; 
+//     } else {
+//         i = 0; 
+//     }
+//     setTimeout("loopThrough()", speed); 
+// }
+
+// window.onload = loopThrough; 
