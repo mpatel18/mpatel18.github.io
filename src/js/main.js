5e3217492cd1905d2d8949cabc26e9c2ca0990d6
Meekit Patel <meekitpatel@gmail.com>
2020-10-14 19:49:15 -0400

    add collapse to navlink click

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+// Collapse on Navbar click
+$('.navbar-nav li a').on('click', function(){
+    if(!$( this ).hasClass('dropdown-toggle')){
+        $('.navbar-collapse').collapse('hide');
+    }
+});
